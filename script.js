9a2abbd4ef2dbec8a74d74fd1f1f851e4e20ded6
Naheed Awan <nadsawan@yahoo.com>
2022-01-24 18:59:02 -0500

    adding background music and organizing

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ score = 0
 // creating Boolean variable
 cross = true;
 
+// creatin window reload function to restart game
+function fun() {
+    location.reload();
+}
+// attaching audio files
+audio = new Audio('Dragon-Castle.mp3')
+audio.play()
+
 // creating on keyDown function for arrows keys
 
 document.onkeydown = function (e) {
     console.log("The key code is:", e.keyCode)
 
-    // moving character upward
-    if (e.keyCode === 38) {
-        document.querySelector('.player').classList.add('animatePlayer')
-        setTimeout(() => {
-            player.classList.remove('animatePlayer')
-        }, 1000)
-    }
 
     // moving character forward
-    else if (e.keyCode === 39) {
+    if (e.keyCode === 39) {
         player = document.querySelector('.player');
         playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
         player.style.left = playerX + 80 + 'px'
@@ -29,6 +30,14 @@ document.onkeydown = function (e) {
         playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
         player.style.left = playerX - 80 + 'px'
     }
+    // moving character upward
+    else if (e.keyCode === 38) {
+        document.querySelector('.player').classList.add('animatePlayer')
+        setTimeout(() => {
+            player.classList.remove('animatePlayer')
+        }, 1000)
+    }
+
 }
 
 // Detection of collision and announcing Game Over
@@ -38,41 +47,42 @@ setInterval(() => {
     player = document.querySelector('.player');
     gameOver = document.querySelector('.gameOver');
     hurdle = document.querySelector('.hurdle')
-    // score = document.querySelector('#scoreCan')
 
-    pX  = parseInt(window.getComputedStyle(player, null).getPropertyValue('left'))
-    
-    pY  = parseInt(window.getComputedStyle(player, null).getPropertyValue('top'))
+    // detecting players position around x and y axis
+    pX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
+    pY = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
+    // deteccting hurdle position around x and y axis
 
-    hX = parseInt(window.getComputedStyle(hurdle, null).getPropertyValue('left'))
-    hY = parseInt(window.getComputedStyle(hurdle, null).getPropertyValue('top'))
+    hX = parseInt(window.getComputedStyle(hurdle).getPropertyValue('left'))
+    hY = parseInt(window.getComputedStyle(hurdle).getPropertyValue('top'))
 
     // adding Math.abs to calculate offset values
-    offsetX = Math.abs(pX - hX)
-    offsetY = Math.abs(pY - hY)
+    X = Math.abs(pX - hX)
+    Y = Math.abs(pY - hY)
     // console.log(offsetY, offsetX)
 
     //  If collision Detected make game over visible
-    if (offsetX < 150 && offsetY < 40) {
+    if (X < 100 && Y < 40) {
         gameOver.style.visibility = 'visible'
         hurdle.classList.remove("animateDragon")
     }
 
     // if no collision , update score by 1
-    else if (offsetX < 140 && cross) {
+    else if (X < 100 && cross) {
         score += 1
         updateScore(score)
         cross = false;
 
-        // making cross true again after 1 sec to update score accurately
+        // making cross true again after 1 sec to update score accurately otherwise it kept on updating 
         setTimeout(() => {
             cross = true;
         }, 1000)
 
-        // after everycross over decrease duration of hurdle
-        // decreaseDuration = a = parseFloat(window.getComputedStyle(hurdle, null).getPropertyValue('animation.duration'))
-        // newDuration = b = a - 0.8
-        // hurdle.style.animationDuration = b + 's'
+        // after every crossover decrease duration of hurdle
+        decreaseDuration = a = parseFloat(window.getComputedStyle(hurdle).getPropertyValue('animation-duration'))
+        newDuration = b = a - 0.2
+        hurdle.style.animationDuration = b + 's'
+        console.log(a, b)
 
     }
 
@@ -92,6 +102,3 @@ function updateScore(score) {
 
 //  }
 
-function fun() {
-    location.reload();
-    }
