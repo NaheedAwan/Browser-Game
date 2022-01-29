2c141a5457c20d2942aebd0f0e11291c013bfce6
Naheed Awan <nadsawan@yahoo.com>
2022-01-29 09:55:01 -0500

    creating readme

--- script.js
+++ script.js
@@ -15,60 +15,62 @@ audio.play()
 
 document.onkeydown = function (e) {
     console.log("The key code is:", e.keyCode)
+    if (!e.repeat) {
 
+        // moving character forward
+        if (e.keyCode === 39) {
+            player = document.querySelector('.player');
+            playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
+            player.style.left = playerX + 80 + 'px'
+        }
 
-    // moving character forward
-    if (e.keyCode === 39) {
-        player = document.querySelector('.player');
-        playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
-        player.style.left = playerX + 80 + 'px'
+        //moving character backward
+        else if (e.keyCode === 37) {
+            player = document.querySelector('.player');
+            playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
+            player.style.left = playerX - 80 + 'px'
+        }
+        // moving character upward
+        else if (e.keyCode === 38) {
+            document.querySelector('.player').classList.add('animatePlayer')
+            setTimeout(() => {
+                player.classList.remove('animatePlayer')
+            }, 1000)
+        }
     }
-
-    //moving character backward
-    else if (e.keyCode === 37) {
-        player = document.querySelector('.player');
-        playerX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
-        player.style.left = playerX - 80 + 'px'
-    }
-    // moving character upward
-    else if (e.keyCode === 38) {
-        document.querySelector('.player').classList.add('animatePlayer')
-        setTimeout(() => {
-            player.classList.remove('animatePlayer')
-        }, 1000)
-    }
-
 }
 
 // Detection of collision and announcing Game Over
 
 
 setInterval(() => {
-    player = document.querySelector('.player');
-    gameOver = document.querySelector('.gameOver');
-    hurdle = document.querySelector('.hurdle')
+    const player = document.querySelector('.player');
+    const gameOver = document.querySelector('.gameOver');
+    const hurdle = document.querySelector('.hurdle')
 
     // detecting players position around x and y axis
     pX = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
     pY = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
-    // deteccting hurdle position around x and y axis
 
+    // deteccting hurdle position around x and y axis
     hX = parseInt(window.getComputedStyle(hurdle).getPropertyValue('left'))
     hY = parseInt(window.getComputedStyle(hurdle).getPropertyValue('top'))
 
     // adding Math.abs to calculate offset values
     X = Math.abs(pX - hX)
     Y = Math.abs(pY - hY)
-    // console.log(offsetY, offsetX)
+    // console.log(Y, X)
 
     //  If collision Detected make game over visible
-    if (X < 100 && Y < 40) {
+    if (X < 80 && Y < 40) {
         gameOver.style.visibility = 'visible'
         hurdle.classList.remove("animateDragon")
+        audio.pause()
     }
+ 
 
     // if no collision , update score by 1
-    else if (X < 100 && cross) {
+    else if (X < 80 && cross) {
         score += 1
         updateScore(score)
         cross = false;
@@ -78,21 +80,28 @@ setInterval(() => {
             cross = true;
         }, 1000)
 
-        // after every crossover decrease duration of hurdle
-        decreaseDuration = a = parseFloat(window.getComputedStyle(hurdle).getPropertyValue('animation-duration'))
-        newDuration = b = a - 0.2
-        hurdle.style.animationDuration = b + 's'
-        console.log(a, b)
-
+        // after every crossover decrease duration of green character
+        const duration = parseFloat(window.getComputedStyle(hurdle).getPropertyValue('animation-duration'))
+        if (duration >= 1.6) {
+            newDuration = duration - 0.1
+            hurdle.style.animationDuration = newDuration + 's'
+            console.log(duration, newDuration)
+        }
+        if (score === 4) {
+            gameWon.style.visibility = 'visible'
+            hurdle.classList.remove("animateDragon")
+        }
     }
-
-}, 100)
+  
+}, 10)
 
 // function for updating scores
 
 function updateScore(score) {
     scoreContainer = document.querySelector('#scoreContainer')
     scoreContainer.innerHTML = "Your score:" + score
+
+
 }
 
 // restart = document.getElementById("#restartButton").addEventListener("click", gameRestart)
